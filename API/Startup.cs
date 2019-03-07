9f277000dd29e95684ef319c288556186f6f2e7c
travchan <travischaan@hotmail.com>
2019-03-06 23:21:36 -0800

    fixed problem after renaming

--- API/Startup.cs
+++ API/Startup.cs
@@ -69,7 +69,8 @@ namespace GIS_API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseCors(options => options.WithOrigins("http://localhost:4200"));
             app.UseHttpsRedirection();
             app.UseMvc();
         }
